a498f35050f4f61e404b0e04b6761afdbe9f9c63
acematty <vesnin95@gmail.com>
2020-09-04 18:51:28 +0300

    complete

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
-	// Place your code here
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"io"
+	"os"
+	"time"
+)
+func main(){
+	ntptime, err := ntp.Time("time.nist.gov")
+	if err != nil {
+		_, err := io.WriteString(os.Stderr, "GfG\n")
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println("current time:", ntptime)
+	fmt.Println("exact time:", time.Now())
 }
