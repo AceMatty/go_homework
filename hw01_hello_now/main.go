01f6013dfda252197088df34710aed99fb3dce92
acematty <vesnin95@gmail.com>
2020-09-05 18:09:40 +0300

    complete7

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-	"time"
-
 	"github.com/beevik/ntp"
+	"time"
 )
 
 func main() {
-	ntptime, err := ntp.Time("time.nist.gov")
+	ntptime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	exect, err := ntp.Query("0.beevik-ntp.pool.ntp.org")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("current time:", ntptime.Format("2006-01-02 15:04:05"))
-	fmt.Println("exact time:", time.Now().Format("2006-01-02 15:04:05"))
+	fmt.Println("exact time:", time.Now().Add(exect.ClockOffset).Format("2006-01-02 15:04:05"))
 }
