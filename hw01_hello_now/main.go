94c5c0f416f008dcdfccbe2034ef79e178cc457f
acematty <vesnin95@gmail.com>
2020-09-05 18:26:19 +0300

    complete10

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -16,7 +16,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	times:= time.Now().Add(exect.ClockOffset)
+	times := time.Now().Add(exect.ClockOffset)
 	fmt.Println("current time:", ntptime.Format("2006-01-02 15:04:05 -0700 MST"))
 	fmt.Println("exact time:", times.Format("2006-01-02 15:04:05 -0700 MST"))
 }
