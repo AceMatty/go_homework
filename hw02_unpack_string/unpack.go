300f8e2a94db4fe35ec5f04071387cc57ba55a71
acematty <vesnin95@gmail.com>
2020-09-12 17:09:24 +0300

    complete1

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,48 @@ package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here
-	return "", nil
+//func main() {
+//	strs := []string{"a4bc2d5e", "abccd", "3abc", "45", "aaa10b", "", "aaa0b"}
+//	for ind := range strs {
+//		str, err := Unpack(strs[ind])
+//		if err != nil {
+//			log.Println(err)
+//		}
+//		fmt.Println(str)
+//	}
+//
+//}
+func Unpack(str string) (string, error) {
+	var strb strings.Builder
+	if len(str) == 0 {
+		return "", nil
+	}
+	if unicode.IsDigit(rune(str[0])) {
+		return "", ErrInvalidString
+	}
+	for i := 0; i < len(str); i++ {
+		if i+1 < len(str) {
+			if unicode.IsDigit(rune(str[i+1])) {
+				if !unicode.IsDigit(rune(str[i+2])) {
+					dig, _ := strconv.Atoi(string(str[i+1]))
+					strb.WriteString(strings.Repeat(string(str[i]), dig))
+				} else {
+					return "", ErrInvalidString
+				}
+			} else if !unicode.IsDigit(rune(str[i])) {
+				strb.WriteString(string(str[i]))
+			}
+		}
+		if i == len(str)-1 && !unicode.IsDigit(rune(str[i])) {
+			strb.WriteString(string(str[i]))
+		}
+	}
+	return strb.String(), nil
 }
